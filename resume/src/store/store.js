9bc11d71c762fdbb238709b6bc16ad9dca76c00e
Maxi-hub <54219318+Maxi-hub@users.noreply.github.com>
2025-01-14 17:00:29 +0300

    Delete resume directory

--- resume/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from './ThemeSlice';
-
-export const store = configureStore({
-    reducer: {
-        theme: themeReducer,
-    }
-});
